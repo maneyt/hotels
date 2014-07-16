f951629fc84da5cd7c44c14731ad743d3fabee5f
Thomas Maney <thomasjmaney@gmail.com>
2014-07-16 16:06:03 -0400

    fix indentation

--- clean_hotel_row.rb
+++ clean_hotel_row.rb
@@ -6,10 +6,10 @@ class CleanHotelRow
   attr_accessor :hotel_data
 
   def clean
-   hotel_data["Hotel"] = clean_name(hotel_data["Hotel"])
-   hotel_data["City"] = clean_city(hotel_data["City"])
-   hotel_data["Phone Number"] = clean_phone(hotel_data["Phone Number"])
-   hotel_data
+    hotel_data["Hotel"] = clean_name(hotel_data["Hotel"])
+    hotel_data["City"] = clean_city(hotel_data["City"])
+    hotel_data["Phone Number"] = clean_phone(hotel_data["Phone Number"])
+    hotel_data
   end
 
   private
